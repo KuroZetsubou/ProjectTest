99c30c346ecfb9841fb7e630f010618517f3804a
KuroZetsubou <cavam99@gmail.com>
2021-10-13 10:39:02 +0200

    Update: Changed the database name for API service

--- api/config.py
+++ api/config.py
@@ -1,5 +1,5 @@
 # PyMongo connection
-MONGO_DATABASE = "project_test" # change this if you don't like it
+MONGO_DATABASE = "project_test_api" # change this if you don't like it
 MONGO_HOST = 'localhost'
 MONGO_PORT = 27017
 MONGO_USERNAME = ''
