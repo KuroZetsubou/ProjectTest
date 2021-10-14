7d21856772a41088f5d6234340db47f35bb6fcf2
KuroZetsubou <cavam99@gmail.com>
2021-10-14 12:31:12 +0200

    Update: Improved `run.sh` script

--- run.sh
+++ run.sh
@@ -1,13 +1,18 @@
 #!/bin/sh
+echo "RabbitMQ Server start"
 rabbitmq-server -detached
+echo "Web Server"
 sleep 2
 screen -dm serve web/public -p 3000
+echo "API Server"
 sleep 2
 screen -dm python3 api/main.py
+echo "Satellite Server"
 sleep 2
 screen -dm python3 satellite/main.py
+echo "API RabbitMQ Receiver Service"
 sleep 2
 screen -dm python3 api/retreiveSatellitePush.py
+echo "MongoDB"
 sleep 2
 mongod --bind_ip 0.0.0.0
-#screen -dmS webserver serve web/public -p 3000
